50de76549ce38c55548e0c1f647742823dfac0e0
Brian Jun <bmtef.02@gmail.com>
2022-04-24 22:20:15 -0700

    react-pdf template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { PDFViewer } from '@react-pdf/renderer';
+import PDFComponent from './components/PDFComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PDFViewer style={{ position: 'absolute', border: 0, width: '100%', height: '100%' }}>
+        <PDFComponent />
+    </PDFViewer>
+
   );
 }
 
